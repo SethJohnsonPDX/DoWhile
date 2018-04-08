5e6697e02d6f863a0177336fcc7090c01cc855ab
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-04-07 18:27:26 -0700

    commit all

--- DoWhile/Program.cs
+++ DoWhile/Program.cs
@@ -10,6 +10,37 @@ namespace DoWhile
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What year is it?");
+            int yearGuess = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = false;
+
+            do
+            {
+                switch (yearGuess)
+                {
+                    case 2000:
+                        Console.WriteLine("Ahhh Y2K! Just kidding, you're wrong. Try again.");
+                        Console.WriteLine("What year is it?");
+                        yearGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3001:
+                        Console.WriteLine("From the future huh? Why did you bother coming back?");
+                        Console.WriteLine("What year is it?");
+                        yearGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2018:
+                        Console.WriteLine("Boom! Nailed it.");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Incorrect. Try again.");
+                        Console.WriteLine("What year is it?");
+                        yearGuess = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+            Console.Read();
         }
     }
 }
